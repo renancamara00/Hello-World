3748afb65b984f9931fde57aecaaef200e7d626f
Renan Câmara <renan.camara00@gmail.com>
2026-03-01 13:00:03 -0300

    Add console logs to test calculo function

--- teste2.js
+++ teste2.js
@@ -1,4 +1,4 @@
-// calculado da média
+// calculo da média usando função 
 
 function calculo(...numeros) {
     if (numeros.length === 0) return 0
@@ -6,6 +6,7 @@ function calculo(...numeros) {
     return media / numeros.length
 }
 
+console.log(calculo())
 console.log(calculo(4, 6))
 console.log(calculo(10, 20, 30, 40))
 console.log(calculo(5, 10, 15, 20, 30))
